28564e52efddfebad134f291e39d58a38e67f01d
Akinbomi Oluwadamilare <joskinjoseph1@gmail.com>
2023-06-29 02:59:48 +0100

    writingPHPtags

--- index.php
+++ index.php
@@ -5,7 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    <h1>Hello World</h1>
+<body> 
+    <h1>
+        <?php
+        echo "Hello, World"
+        ?>
+    </h1>
 </body>
 </html>
