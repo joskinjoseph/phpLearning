5b70bb8d2ad7555616039520379a4cd90a13b767
Akinbomi Oluwadamilare <joskinjoseph1@gmail.com>
2023-06-29 04:13:52 +0100

    Conditionals and Booleans in PHP

--- index.php
+++ index.php
@@ -4,14 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DEMO</title>
+    <style>
+        body {
+    display: grid;
+    place-items: center;
+    height: 100vh;
+    margin: 0;
+    font-family: sans-serif;
+}
+    </style>
 </head>
 <body> 
+    <?php
+        $name = "Women of Owu";
+        $read = false;
+
+        if ($read) {
+         $message = "You have read $name";   
+        } else {
+            $message = "You have NOT read $name";   
+        }
+    ?>    
     <h1>
-        <?php
-        $salutation = "Hello";
-        echo $salutation." " .  "Buhari"
-        echo "$salutation Idans!"
-        ?>
+      <?php echo $message; ?>"
+      <!-- //shortcut to write -->
+      <?= $message;  ?>
     </h1>
 </body>
 </html>
