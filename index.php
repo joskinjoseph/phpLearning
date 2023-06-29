8b9bdde3c22a39b7aa70c96f1111cb74398d56dc
Akinbomi Oluwadamilare <joskinjoseph1@gmail.com>
2023-06-29 04:50:54 +0100

    Learning Array (collection of names, things etc)

--- index.php
+++ index.php
@@ -4,31 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DEMO</title>
-    <style>
-        body {
-    display: grid;
-    place-items: center;
-    height: 100vh;
-    margin: 0;
-    font-family: sans-serif;
-}
+
     </style>
 </head>
 <body> 
-    <?php
-        $name = "Women of Owu";
-        $read = false;
-
-        if ($read) {
-         $message = "You have read $name";   
-        } else {
-            $message = "You have NOT read $name";   
-        }
-    ?>    
+   
     <h1>
-      <?php echo $message; ?>"
-      <!-- //shortcut to write -->
-      <?= $message;  ?>
+      Recommended Books
     </h1>
+    <?php
+        $books = [
+            "Music and Cultural Heritage of the Yorubas",
+            "Harvest of Corruption",
+            "Scale and Speed"
+        ];
+    ?>
+    <ul>
+        <?php foreach($books as $book) {
+            echo "<li>{$book}</li>";
+        }
+        ?>
+    </ul>
+    <ul>
+        <?php foreach ($books as $book) : ?>
+            <li><?= $book; ?> </li>
+            <?php endforeach; ?>
+    </ul>
 </body>
 </html>
