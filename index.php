7d24fee30f8f30c9b04b5cd355f62b65321e74e3
Akinbomi Oluwadamilare <joskinjoseph1@gmail.com>
2023-07-08 16:46:55 +0100

    2nd Attempt

--- index.php
+++ index.php
@@ -66,6 +66,42 @@
         return $pupil['age'] <= 9;
     });
    ?>
+
+<?php
+        $books = [
+            [
+               'name'=> "Music and Cultural Heritage of the Yorubas",
+               'author'=> "Dr. Ajenifuja",
+              'publishUrl'=>  'http://www.jjjjdghs.com',
+              'publishYear'=> 2010
+            ],
+            [
+                'name'=>  "Harvest of Corruption",
+                'author'=>  "William Shakespare",
+                'publishUrl'=> "http://www.jjjjdghs.com",
+                'publishYear'=> 1999
+            ],
+            [
+                'name'=> "Scale and Speed",
+                'author'=> "Prof. Akinbomi",
+                'publishUrl'=> "http://www.jjjjdghs.com",
+                'publishYear'=> 2023
+            ],
+            [
+                'name'=> "Becoming a Musician",
+                'author'=> "Prof. Akinbomi",
+                'publishUrl'=> "http://www.jjjjdghs.com",
+                'publishYear'=> 2055
+            ],
+        ];
+        $filterBooks = array_filter($books, function ($book) {
+            return $book['author'] === 'Prof. Akinbomi';
+        })
+?>
+
+
+
+
     <ul>
         <?php foreach ($filterPupils as $pupil) : ?>
             <li>
@@ -74,6 +110,17 @@
                     ( <?= $pupil['yearOfAdmin']; ?>) - Class <?= $pupil['class']; ?>
                 </a>
 
+            </li>
+            <?php endforeach; ?>
+    </ul>
+
+    <ul>
+        <?php foreach($filterBooks as $book) : ?>
+            <li>
+                <a href="publishYear">
+                    <?= $book['name']; ?>
+                    (<?= $book['publishYear']; ?>) -By <?= $book['author']; ?>
+                </a>
             </li>
             <?php endforeach; ?>
     </ul>
