c0999cca289e52f781f79afd790f8b1a4b0e8267
Akinbomi Oluwadamilare <joskinjoseph1@gmail.com>
2023-07-19 19:56:33 +0100

    hhh

---
+++ oop.php
@@ -0,0 +1,16 @@
+
+<?php
+
+class Customer
+{
+	public $name;
+
+	public function getName()
+	{
+		return $this->name;
+	}
+}
+
+$customer = new Customer();
+$customer->name = 'Bob';
+echo $customer->getName(); // Bob
