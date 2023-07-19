d3b02d3992ff83924f2427c82354891ec073ddb5
Akinbomi Oluwadamilare <joskinjoseph1@gmail.com>
2023-07-19 01:55:47 +0100

    Environment and configuration

--- index.php
+++ index.php
@@ -2,17 +2,20 @@
 
     require 'function.php';
 
+    require 'Database.php';
+
     // require 'router.php';
 
-    // connect to our MySQL databass
-   
-$dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
-   $pdo = new PDO($dsn, 'oluwadamilare', '12345@Oluwadamilare');
+   $config = require('config.php');
+    
+    
+    $db = new Database($config['database']);
+    
+    $posts = $db->query("select * from posts")->fetchAll(PDO::FETCH__ASSOC);
 
-   $pdo->prepare("select * from posts");
-   $statement->execute();
+    dd($posts);
 
-   $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
 
    foreach ($post as $post) {
     echo "<li>" . $post['title'] .  "</li>";
