203693ccadb09cd08dbf65114003d906e4030164
Akinbomi Oluwadamilare <joskinjoseph1@gmail.com>
2023-08-07 15:08:05 +0100

    Real PDO first step

--- index.php
+++ index.php
@@ -1,19 +1,27 @@
 <?php
 
-    require 'function.php';
+// require 'function.php';
 
-    require 'Database.php';
+//   require 'Database.php';
+//     // require 'router.php';
 
-    require 'router.php';
+//    $config = require('config.php');
 
-   $config = require('config.php');
-    
-    
-    $db = new Database($config['database']);
-    
-    $posts = $db->query("select * from posts")->fetchAll(PDO::FETCH__ASSOC);
+//     $db = new Database($config['database']);
 
-    dd($posts);
+//     $posts = $db->query("select * from posts")->fetchAll(PDO::FETCH__ASSOC);
 
+//     dd($posts);
 
-   
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
+
+$pdo = new PDO($dsn, 'oluwadamilare', '12345@Oluwadamilare');
+
+$statement = $pdo->prepare("select * from man");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['name'] . "</li>";
+}
