310bb6457b87260f6da42d49461966a5b2bc0020
Akinbomi Oluwadamilare <joskinjoseph1@gmail.com>
2023-11-01 23:00:12 +0100

    MANAGING PASSWORD

--- views/index.view.php
+++ views/index.view.php
@@ -4,7 +4,7 @@
 <?php require "partials/banner.php" ?>
   <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-     <p>My Website</p>
+     <p>Hello <?= $_SESSION['user']['email'] ?? 'Guest'?> </p>
     </div>
   </main>
 <?php require 'partials/footer.php'?>
